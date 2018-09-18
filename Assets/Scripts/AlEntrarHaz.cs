d884330be33c4df42a3d3b44fb62d42596ff2363
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-09-18 00:02:43 -0300

    Incorporado sonido al colisionar con la bola

--- Assets/Scripts/AlEntrarHaz.cs
+++ Assets/Scripts/AlEntrarHaz.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class AlEntrarHaz : MonoBehaviour {
 
-    void OnTriggerEnter(Collider other)
+    public AudioClip clip;
+    float volumen = 1.0f;
+    Transform posicion;
+
+    void Start ()
     {
+        posicion = transform;
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        AudioSource.PlayClipAtPoint(clip, posicion.position, volumen);
         Destroy(this.gameObject);
     }
 }
