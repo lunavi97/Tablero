8a48745a1f8d59cd843430a5786d30afe8af1832
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-09-17 22:04:23 -0300

    Agregado controlador de tablero que detecte las teclas pulsadas

---
+++ Assets/Scripts/ControladorTablero.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorTablero : MonoBehaviour {
+    
+	void Update () {
+		if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("Tecla Arriba pulsada");
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Tecla Izquierda pulsada");
+        }
+
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("Tecla Abajo pulsada");
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Tecla Derecha pulsada");
+        }
+	}
+}
