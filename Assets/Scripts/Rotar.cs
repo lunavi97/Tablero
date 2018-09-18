3c665d2aeb8b675839da56fef018b4da025fbcab
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-09-17 23:15:41 -0300

    Rotadas paredes móviles

---
+++ Assets/Scripts/Rotar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotar : MonoBehaviour {
+
+    public Vector3 direccion;
+
+	void Update () {
+        transform.Rotate(direccion);
+	}
+}
